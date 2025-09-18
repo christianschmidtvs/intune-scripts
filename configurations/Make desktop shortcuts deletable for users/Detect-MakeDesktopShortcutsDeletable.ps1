eb1ab35ad39cb88d1a09ad51990a733df091d20a
Christian Schmidt <christian.schmidt.vs@outlook.de>
2025-09-18 22:14:13 +0200

    Added

---
+++ configurations/Make desktop shortcuts deletable for users/Detect-MakeDesktopShortcutsDeletable.ps1
@@ -0,0 +1,20 @@
+﻿$defaultProfileDesktopPath = "C:\Users\Public\Desktop\"
+$shortcutsToExclude = @()
+
+$shortcuts = Get-ChildItem -Path $defaultProfileDesktopPath -Filter "*.lnk" | Where-Object { $_.Name -notin $shortcutsToExclude }
+$compliant = $true
+$shortcuts | ForEach-Object {
+    $objectAcl = Get-Acl -Path ($_.FullName)
+    If ($objectAcl.Sddl -notlike "*(A;;0x1301bf;;;BU)*") {
+        $compliant = $false
+    }
+}
+
+If ($compliant) {
+    Write-Output "Compliant"
+    Exit 0
+}
+Else {
+    Write-Output "Non-Compliant"
+    Exit 1
+}
---
+++ configurations/Make desktop shortcuts deletable for users/Remediate-MakeDesktopShortcutsDeletable.ps1
@@ -0,0 +1,11 @@
+﻿$defaultProfileDesktopPath = "C:\Users\Public\Desktop\"
+$shortcutsToExclude = @()
+
+$shortcuts = Get-ChildItem -Path $defaultProfileDesktopPath -Filter "*.lnk" | Where-Object { $_.Name -notin $shortcutsToExclude }
+$shortcuts | ForEach-Object {
+    $objectAcl = Get-Acl -Path ($_.FullName)
+    $localUsersGroup = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-545")
+    $aclRule = New-Object System.Security.AccessControl.FileSystemAccessRule($localUsersGroup, "Modify", "Allow")
+    $objectAcl.SetAccessRule($aclRule)
+    Set-Acl -Path ($_.FullName) -AclObject $objectAcl
+}
