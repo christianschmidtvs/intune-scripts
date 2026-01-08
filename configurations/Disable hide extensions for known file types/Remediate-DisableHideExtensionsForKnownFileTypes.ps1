ad4c0795f208eed18fcea8d6bd7261fbd221174e
Christian Schmidt <christian.schmidt.vs@outlook.de>
2026-01-08 17:06:35 +0100

    Cleanup

--- configurations/Disable hide extensions for known file types/Remediate-DisableHideExtensionsForKnownFileTypes.ps1
+++
@@ -1,6 +0,0 @@
-$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-$registryPropertyName = "HideFileExt"
-$registryPropertyValue = 0
-If ((Get-ItemProperty -Path $registryPath -Name $registryPropertyName).$registryPropertyName -ne $registryPropertyValue) {
-    Set-ItemProperty -Path -Name $registryPropertyName -Value $registryPropertyValue
-}
